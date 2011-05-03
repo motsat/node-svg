07954792603c5c95ad615db4e132313332581d54
motsat <konpeiex@gmail.com>
2011-05-03 18:13:54 +0900

    obj create and drag

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var io            = require('socket.io'),
     express       = require('express'),
-    messages      = [],
-    messageType   = {'GET_DATA' : 0, 'STRING' : 1} ;    // GET=取得要求、 STRING=チャットメッセージ
-
-
+    objects      = [],
+    ACTION_TYPE   = {'REQUEST_CREATE' : 1,  // クライアントからの作成要求
+                    'CREATE'         : 2,  // クラインとへの作成指示（REQUEST_CREATE後)
+                    'REQUEST_LOCK'   : 3,  // サーバへのロック要求
+                    'LOCK'           : 4,   // クラインとへのロック指示(REQUEST_EDIT)を要求したクライアント以外に送るもの
+                    'REQUEST_EDIT'   : 5,  // サーバへの編集要求
+                    'EDIT'           : 6   // クライントへの編集指示（REQUEST_EDIT後)
+                    };
 var app = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -25,12 +29,12 @@ socket.on('connection', function(client){
 
 function addMessage(msg)
 {
-  msg.id        = messages.length + 1;
+  msg.id        = objects.length + 1;
   msg.createdAt = getNowDate();
   if (msg.option.color == '') {
     msg.option.color = 'black';
   }
-  messages.push(msg);
+  objects.push(msg);
 
   return msg;
 }
@@ -39,7 +43,7 @@ function addMessage(msg)
 function onDisconnect(client)
 {
   msg  = {"data"      : client. sessionId+"が切断されました",
-          "type"      : messageType.STGING,
+          "type"      : ACTION_TYPE.STGING,
           "option"    : {"color" : "red"},
           "createdAt" : getNowDate()} ;
   //socket.broadcast(msg);
@@ -48,18 +52,40 @@ function onDisconnect(client)
 function onMessage(msg, client)
 {
   console.log(msg);
-  client.broadcast(msg);
-  //if (msg.type == messageType.GET_DATA) {
-  //  if (typeof msg.more_id == 'number') {
-  //    client.send(messages.slice(msg.more_id));
-  //  }
-  //} else {
-  //  addMessage(msg);
-  //  client.send(msg);
-  //  client.broadcast(msg);
-  //}
-}
+  //client.broadcast(msg);
 
+/*
+サーバ側のオブジェクトのstatus、attrかえる処理未実装
+
+*/
+  switch (msg.action) {
+  case ACTION_TYPE.REQUEST_CREATE:
+    var obj = {
+      "id"      : objects.length + 1,
+      "action"  : ACTION_TYPE.CREATE,
+      "type"    : msg.type,
+      "status"  : '',
+      "clientId": client. sessionId};
+    objects.push(obj);
+    client.send(obj);
+    client.broadcast(obj);
+    break;
+  case ACTION_TYPE.REQUEST_LOCK:
+    // fix me
+    var obj = {
+      "action": ACTION_TYPE.LOCK,
+      "id"    : msg.id};
+    client.broadcast(obj);
+    break;
+  case ACTION_TYPE.REQUEST_EDIT:
+    var obj = {
+      "action": ACTION_TYPE.EDIT,
+      "attr": msg.attr,
+      "id"    : msg.id};
+    client.broadcast(obj);
+    break;
+  }
+}
 function getNowDate(){
   d = new Date();
   return  d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate()+ " "+ d.getHours()+':' +d.getMinutes()+':' + d.getSeconds();
